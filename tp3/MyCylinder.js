18b5fbec12834c59a0294cd45d08f0e5da65381f
Rodrigo Póvoa <up202108890@up.pt>
2024-03-05 17:54:12 +0000

    tp3 done

--- tp3/MyCylinder.js
+++ tp3/MyCylinder.js
@@ -1,10 +1,10 @@
 import { CGFobject } from '../lib/CGF.js';
 /**
- * MyPrism
+ * MyCylinder
  * @constructor
  * @param scene - Reference to MyScene object
  */
-export class MyPrism extends CGFobject {
+export class MyCylinder extends CGFobject {
     constructor(scene, slices, stacks) {
         super(scene);
         this.slices = slices;
@@ -17,7 +17,26 @@ export class MyPrism extends CGFobject {
         this.indices = [];
         this.normals = [];
     
-        
+        let angle = 2 * Math.PI / this.slices;
+
+        for(let stack = 0; stack <= this.stacks; stack++) {
+            for(let slice = 0; slice < this.slices; slice++) {
+                let x = Math.cos(slice * angle);
+                let y = Math.sin(slice * angle);
+                this.vertices.push(x, y, stack / this.stacks);
+                this.normals.push(x, y, 0);
+            }
+        }
+
+        for(let stack = 0; stack < this.stacks; stack++) {
+            for(let slice = 0; slice < this.slices; slice++) {
+                let first = stack * this.slices + slice;
+                let second = first + this.slices;
+                this.indices.push(first, second + 1, second);
+                this.indices.push(first, first + 1, second + 1);
+            }
+        }
+
         
         this.primitiveType = this.scene.gl.TRIANGLES;
     
