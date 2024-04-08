db0aec12c742831e7d9271b7235535ba6b656ae6
Rodrigo Póvoa <up202108890@up.pt>
2024-04-08 11:10:03 +0100

    tp5 done and project startup

---
+++ project/MySphere.js
@@ -0,0 +1,16 @@
+import {CGFobject} from '../lib/CGF.js';
+
+export class MySphere extends CGFobject {
+
+    constructor(scene, radius, slices, stacks, inside=false, north=1, south=1) {
+        super(scene);
+        this.radius = radius;
+        this.slices = slices;
+        this.stacks = stacks;
+        this.inside = inside ? -1 : 1;
+        this.north = north;
+        this.south = south;
+        this.initBuffers();
+    }
+    
+}
